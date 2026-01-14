8ca8d25b163dc814cc38feb8126d6c18d5bc96a1
xavier kappeler <the.suisse.gold.prospector@gmail.com>
2026-01-14 11:05:44 +0000

    File changes

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,10 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/google-genai';
+import { config } from 'dotenv';
+
+config({ path: `.env.local` });
+config();
+
 
 export const ai = genkit({
   plugins: [googleAI()],
