0a6639533ea36aecfd3e2124b463551b77491246
xavier kappeler <the.suisse.gold.prospector@gmail.com>
2026-01-14 09:49:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1063796498-c439e",
+  "appId": "1:531203499316:web:ce2acb291b5aa66ee23161",
+  "apiKey": "AIzaSyDiUEXg8n_z2KhIwxvgBVX98g7xmuM5xCU",
+  "authDomain": "studio-1063796498-c439e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "531203499316"
+};
